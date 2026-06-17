36352ebb31d5852705a1eba5f3ec6025efb5deef
Abinash <abinashsubudhi213@gmail.com>
2026-06-18 03:13:38 +0530

    dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,25 @@
+import { useState } from "react";
+import api from "../services/api";
+
 export default function Dashboard() {
+  const [streamInfo, setStreamInfo] = useState(null);
+
+  const handleGoLive = async () => {
+    try {
+      const res = await api.post("/stream");
+
+      setStreamInfo({
+        streamKey: res.data.streamKey,
+        rtmpUrl: res.data.rtmpUrl.replace(
+          "SERVER_IP",
+          "54.91.27.248"
+        )
+      });
+    } catch (err) {
+      alert("Failed to create stream");
+    }
+  };
+
   return (
     <div className="min-h-screen bg-black text-white p-8">
       <h1 className="text-4xl mb-8">Streamer Dashboard</h1>
@@ -17,9 +38,19 @@ export default function Dashboard() {
         </div>
       </div>
 
-      <button className="mt-8 px-6 py-3 bg-red-600 rounded-xl">
+      <button
+        onClick={handleGoLive}
+        className="mt-8 px-6 py-3 bg-red-600 rounded-xl"
+      >
         Go Live
       </button>
+
+      {streamInfo && (
+        <div className="mt-8 bg-zinc-900 p-6 rounded-xl">
+          <p><b>RTMP URL:</b> {streamInfo.rtmpUrl}</p>
+          <p><b>Stream Key:</b> {streamInfo.streamKey}</p>
+        </div>
+      )}
     </div>
   );
 }
